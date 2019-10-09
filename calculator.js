640f2856533b5d3423ea1627aeecec9e45699144
eugewelker <55256275+eugewelker@users.noreply.github.com>
2019-10-09 21:02:56 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,32 @@
+function Calculator()
+{
+  var answer= document.querySelector('#answer');
+  var num1= Number(document.querySelector('#num1').value);
+  var num2= Number(document.querySelector('#num2').value);
+  var select= document.querySelector('select');
+  var operator= select.options[select.selectedIndex].value;
+  var theCalculatedAnswer;
+
+  if(operator=="add")
+  {
+  theCalculatedAnswer= num1+num2;
+  }
+  else if(operator=="minus")
+  {
+  theCalculatedAnswer= num1-num2;
+  }
+  else if(operator=="divide")
+  {
+  theCalculatedAnswer= num1/num2;
+  }
+  else if (operator=="multiply")
+  {
+    theCalculatedAnswer= num1*num2;
+  }
+  else
+  {
+    alert("please select an operator");
+    theCalculatedAnswer="invalid operator";
+  }
+  answer.innerHTML=theCalculatedAnswer;
+}
